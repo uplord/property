124f0dc5de7d0a44f0a4d2cf3a34e3e56e74af42
Michael <michael@uplord.co.uk>
2024-03-19 16:11:43 +0000

    env variables

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: ["@pinia/nuxt", 'nuxt-strapi-blocks-renderer'],
+  runtimeConfig: {
+    public: {
+      strapiApi: process.env.STRAPI_API,
+      strapiBearer: process.env.STRAPI_BEARER
+    }
+  }
   srcDir: "src/",
 })
--- src/store/properties.js
+++ src/store/properties.js
@@ -3,8 +3,10 @@ import axios from 'axios';
 
 export const usePropertiesStore = defineStore("propertiesStore", () => {
 
+  const config = useRuntimeConfig()
+
   const headers = {
-    'Authorization': 'Bearer bc171b6b354b09162295aefb2c2e19559aa2340035506eb008875ceaf28f54f2c53ee3d4db2b5552f558df7571e05dc4e26fa83a03fcc16f017ed2b687d1ad17688e1b1c39820ce878221ff4da98545d1407188b6e26cb26700cbefaaca418f9ba6458ae55ce4f135c39b721c08f2cc224c8836608b8c91133301b1a9c2ab422'
+    'Authorization': 'Bearer ' + config.public.strapiBearer
   };
 
   const state = reactive({
@@ -23,7 +25,7 @@ export const usePropertiesStore = defineStore("propertiesStore", () => {
         this.pagination = {... this.pagination, ...pagination};
       }
 
-      await axios.get('http://localhost:1337/api/properties?pagination[page]=' + this.pagination.page + '&pagination[pageSize]=' + this.pagination.pageSize, {
+      await axios.get(config.public.strapiApi + '/properties?pagination[page]=' + this.pagination.page + '&pagination[pageSize]=' + this.pagination.pageSize, {
         headers
       }).then(response => {
         this.properties = response.data.data;
@@ -31,7 +33,7 @@ export const usePropertiesStore = defineStore("propertiesStore", () => {
       });
     },
     async fetchProperty(id) {
-      await axios.get('http://localhost:1337/api/properties/' + id, {
+      await axios.get(config.public.strapiApi + '/properties/' + id, {
         headers
       }).then(response => {
         this.property = response.data.data;
