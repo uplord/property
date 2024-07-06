c71cee4f51d56f34243c669728689e2854d09e43
Michael <michael@uplord.co.uk>
2024-07-06 15:55:44 +0100

    test

--- src/pages/test.vue
+++ src/pages/test.vue
@@ -2,7 +2,18 @@
     <div>
         <h1>DeviceMotion Event Demo</h1>
     <p>Move your device to see the motion data.</p>
-        <p>{{ errorMessage }}</p>
+    <div id="output">
+      Accelerometer: <br>
+      x: <span>{{ accelX }}</span> <br>
+      y: <span>{{ accelY }}</span> <br>
+      z: <span>{{ accelZ }}</span> <br>
+      <br>
+      Gyroscope: <br>
+      alpha: <span>{{ gyroAlpha }}</span> <br>
+      beta: <span>{{ gyroBeta }}</span> <br>
+      gamma: <span>{{ gyroGamma }}</span> <br>
+      <p v-if="errorMessage">{{ errorMessage }}</p>
+    </div>
         <div @click="checkDeviceMotionPermission()">Button</div>
     </div>
   </template>
@@ -11,38 +22,70 @@
 import { ref, onMounted, onUnmounted } from 'vue';
 export default {
   setup() {
+    const accelX = ref('0');
+    const accelY = ref('0');
+    const accelZ = ref('0');
+    const gyroAlpha = ref('0');
+    const gyroBeta = ref('0');
+    const gyroGamma = ref('0');
     const errorMessage = ref('');
+
+    function handleMotion(event) {
+      if (event.acceleration) {
+        accelX.value = event.acceleration.x ? event.acceleration.x.toFixed(2) : 'N/A';
+        accelY.value = event.acceleration.y ? event.acceleration.y.toFixed(2) : 'N/A';
+        accelZ.value = event.acceleration.z ? event.acceleration.z.toFixed(2) : 'N/A';
+      }
+
+      if (event.rotationRate) {
+        gyroAlpha.value = event.rotationRate.alpha ? event.rotationRate.alpha.toFixed(2) : 'N/A';
+        gyroBeta.value = event.rotationRate.beta ? event.rotationRate.beta.toFixed(2) : 'N/A';
+        gyroGamma.value = event.rotationRate.gamma ? event.rotationRate.gamma.toFixed(2) : 'N/A';
+      }
+    }
 
     function checkDeviceMotionPermission() {
         
         errorMessage.value = '111';
-        if (typeof DeviceMotionEvent.requestPermission === 'function') {
-            errorMessage.value = 'ask';
-            DeviceMotionEvent.requestPermission()
-                .then(permissionState => {
-                    errorMessage.value = 'permissionState'
-                    if (permissionState === 'granted') {
-                        errorMessage.value = 'Granted'
-                        window.addEventListener('devicemotion', () => {
-                            errorMessage.value = 'Motion'
-                        });
-                    }
-                })
-                .catch(function (error) {
-                    errorMessage.value = 'DeviceMotionEvent not enabled: ' + error;
-                })
+        if (typeof DeviceMotionEvent !== 'undefined') {
+            if (typeof DeviceMotionEvent.requestPermission === 'function') {
+                errorMessage.value = 'ask';
+                DeviceMotionEvent.requestPermission()
+                    .then(permissionState => {
+                        errorMessage.value = 'permissionState'
+                        if (permissionState === 'granted') {
+                            errorMessage.value = 'Granted'
+                            window.addEventListener('devicemotion', handleMotion);
+                        }
+                    })
+                    .catch(function (error) {
+                        errorMessage.value = 'DeviceMotionEvent not enabled: ' + error;
+                    })
+            } else {
+                window.addEventListener('devicemotion', handleMotion);
+            }
         } else {
-            errorMessage.value = 'Error';
+            errorMessage.value = 'Not supported';
         }
     }
 
     onMounted(() => {
+        checkDeviceMotionPermission();
+    });
 
+    onUnmounted(() => {
+      window.removeEventListener('devicemotion', handleMotion);
     });
 
     return {
-      errorMessage,
-      checkDeviceMotionPermission
+        accelX,
+        accelY,
+        accelZ,
+        gyroAlpha,
+        gyroBeta,
+        gyroGamma,
+        errorMessage,
+        checkDeviceMotionPermission
     };
   }
 }
